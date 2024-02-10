a375ec506e855916c50d7a7b66006e2c521daf6f
Ajay Yadav <72748698+ajaygeekcoders@users.noreply.github.com>
2024-02-10 13:28:14 +0530

    Update 1.TwoSum.js

--- leetcode/1.TwoSum.js
+++ leetcode/1.TwoSum.js
@@ -1,4 +1,5 @@
 
+Note: It work only if sum indices are near to each other, look other approach
 /**
 https://leetcode.com/problems/two-sum/description/
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
